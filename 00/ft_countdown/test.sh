c94bccf43d17e8db88cb5215f639c8a1c6b44ceb
Jonghyeok Han <jonghan@c1r17s2.42gyeongsan.kr>
2024-09-01 21:54:59 +0900

    2

--- 00/ft_countdown/test.sh
+++ 00/ft_countdown/test.sh
@@ -5,9 +5,9 @@ cc -Wall -Werror -Wextra ft_countdown.c -o ft_countdown
 ./ft_countdown | cat -e > output
 
 if diff -c output answer > /dev/null; then
-    echo "\033[0;32mOK:)\033[0m"
+    echo -e "\033[0;32mOK:)\033[0m"
 else
-    echo "\033[0;31mKO:)\033[0m"
+    echo -e "\033[0;31mKO:)\033[0m"
 fi
 
 rm output ft_countdown
